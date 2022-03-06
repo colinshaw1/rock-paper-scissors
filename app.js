b45dd6b347cc2b308e9dfcaf5f3a3732b7832eac
Colin Shaw <colin.shaw400@outlook.com>
2022-03-06 14:25:37 +0000

    Add js

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Self contains all code within the game to one function
+ * Scopes all to the one fucntions
+ */
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro Button");
+        const intoScreen = document.querySelector("intro")
+    }
+}
